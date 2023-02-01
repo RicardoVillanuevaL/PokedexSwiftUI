c84883f0be31404f0c5febd7db7e999d844cee96
Ricardo Villanueva <r.k.villanueva.laurente@gmail.com>
2023-01-31 19:17:23 -0500

    Initial Commit

---
+++ poqueDex/poqueDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  poqueDexApp.swift
+//  poqueDex
+//
+//  Created by RUBEN SOTO on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct poqueDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
